e8d2d7f3d42d184f619a5d5476078c4aaf41a268
Dabeey <abbasdaughter18@gmail.com>
2025-09-12 12:13:02 +0100

    refactor(fetcher): move fetch_data from parser.py to fetcher.py for clearity and reusability

---
+++ fetcher.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def fetch_data(url: str, timeout: float = 10) -> list[dict]:
+    """ 
+    Fetch data from ESPN website.
+
+    ARGS:
+        url (str): The URL of the website.
+        timeout (float): 10 seconds before timeout.
+
+    OUTPUT:
+        Returns a list of dictionary in JSON format.
+
+    """
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+        logging.info(f'Successfully fetched data from {url}')
+        return response.json()
+        
+    except requests.RequestException as e:
+        logging.error(f'An error occurred while fetching data: {e}')
+
--- parser.py
+++ parser.py
@@ -1,32 +1,9 @@
-import requests
 import json
 from typing import Optional
 
 
 
 url = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard"
-
-
-def fetch_data(url: str, timeout: float = 10) -> list[dict]:
-    """ 
-    Fetch data from ESPN website.
-
-    ARGS:
-        url (str): The URL of the website.
-        timeout (float): 10 seconds before timeout.
-
-    OUTPUT:
-        Returns a list of dictionary in JSON format.
-
-    """
-    try:
-        response = requests.get(url, timeout=timeout)
-        response.raise_for_status()
-        logging.info(f'Successfully fetched data from {url}')
-        return response.json()
-        
-    except requests.RequestException as e:
-        logging.error(f'An error occurred while fetching data: {e}')
 
 
 """ 
