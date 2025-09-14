d5faba6c9f350356c7d536ba31f4d37024360b42
Dabeey <abbasdaughter18@gmail.com>
2025-09-14 23:51:39 +0100

    ref(main): import argparse

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from utils.logger import logging
 from fetcher import fetch_data
 from parser import extract_event
 from storage import save_to_json
-
+import argparse
 
 if __name__=='__main__':
-    
+
     url = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard"
 
     data = fetch_data(url) 
@@ -13,4 +13,11 @@ if __name__=='__main__':
     save_to_json(extracted_list)
 
 
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--storage',choices=['json','csv'], default='json')
+    args = parser.parse_args()
+
+    if args.storage == 'json':
+        save_to_json(data)
+    else:
+        save_to_
