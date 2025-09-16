da6232a7a6675410547e7dc503971c8d6978476d
Dabeey <abbasdaughter18@gmail.com>
2025-09-16 14:33:19 +0100

    docs(storage): update docstring for save_to_csv function to include detailed arguments and return information

--- storage.py
+++ storage.py
@@ -26,6 +26,23 @@ def save_to_json(data: Optional[list], filename: str = 'events.json'):
 
 
 def save_to_csv(data: Optional[list], filename='events.csv'):
+    """ 
+    Save extracted data to CSV. 
+
+    Args:
+        data (Optional[list]) : List containing extracted events data.
+        filename (str) : File name to save the data in. Default is 'events.csv'
+
+    Return:
+        None
+
+    Side Effect:
+        Creates or overwrites a CSV file on disk
+    """
+    if data is None:
+        data = []
+        logging.warning('Data does not exist. Check empty list and try again')
+        
     with open(filename,'w',newline='') as file:
         fieldnames = ['id', 'date', 'venue','teams']
         writer = csv.DictWriter(data,fieldnames=fieldnames)
